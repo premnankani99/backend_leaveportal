5cd7a06523f2c341bafeef3f5d2fb55cc553d4cb
premnankani99 <premnankani99@gmail.com>
2026-07-22 18:54:14 +0530

    Cleaned up all temporary testing scripts

--- check_db.ts
+++
@@ -1,6 +0,0 @@
-import prisma from './prismaClient';
-async function main() {
-    const leaves = await prisma.leave_requests.findMany({ orderBy: { created_at: 'desc' }, take: 1 });
-    console.log(JSON.stringify(leaves, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
--- check_db2.ts
+++
@@ -1,2 +0,0 @@
-import prisma from './prismaClient';
-prisma.leave_requests.findMany({ select: { leave_type: true }, take: 5 }).then(console.log).finally(() => prisma.$disconnect());
